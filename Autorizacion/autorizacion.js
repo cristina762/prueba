d7f3f864ec9ed754d2648086229ee4359b9fce2e
unknown <crismolina@gmail.com>
2019-12-19 11:55:41 +0100

    Primera version: Proyecto Autorizacion

---
+++ Autorizacion/autorizacion.js
@@ -0,0 +1,24 @@
+
+function verificar(){
+
+    if(document.getElementById("usuario").value==""){
+        document.getElementById("mensaje").value="Debe escribir un nombre de usuario";
+    }
+
+
+else{
+    if(document.getElementById("usuario").value=="guess"){
+        document.getElementById("mensaje").value="usuario autorizado";
+    }
+
+}
+    
+
+
+function Salir(){
+
+    if(document.getElementById(Salir()){
+    document.getElementById("mensaje").value="EXIT";
+
+}
+}
